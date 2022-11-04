1bd3dbf5b8f40a4ce84da98ccda1fb2d2e17d258
ian <dykas@oakland.edu>
2022-11-04 11:52:04 -0400

    cleanup

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 import { StyleSheet, Text, View } from "react-native";
 import Buttons from "./components/Buttons.js";
 import HUD from "./components/HUD.js";
 import Pet from "./components/Pet.js";
+import Test from "./components/TestNav.js";
 
+//App
 export default function App() {
   return (
     <View style={styles.container}>
       <HUD />
       <Pet />
       <Buttons />
-      <StatusBar style="auto" />
     </View>
   );
 }
---
+++ components/TestNav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+
+const Test = () => {
+  return (
+    <View>
+      <Text>Testing nav</Text>
+    </View>
+  );
+};
+
+export default Test;
